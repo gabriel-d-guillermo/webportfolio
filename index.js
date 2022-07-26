d7ba1aaa98e651420218a014f7533a9d4d967f9d
Gabriel Guillermo <gabby961989@gmail.com>
2022-07-26 11:18:11 +0800

    dev portfolio completed

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const navBar = document.getElementById('navBar');
 
 
-
-
 // add a background color and border color when navbar is scroll
 window.addEventListener('scroll',()=>{
 let current=window.pageYOffset;
@@ -14,7 +12,31 @@ navBar.classList.remove("bg-primary-color" ,"box-shadow");
 }
 });
 
-window.document.getElementById('toggler').addEventListener('click',()=>{	
-	navBar.classList.toggle("box-shadow-2");
+// window.document.getElementById('toggler').addEventListener('click',()=>{	
+// 	navBar.classList.toggle("box-shadow-2");
+// });
+
+
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav ul li");
+window.addEventListener("scroll", () => {
+  let currents = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+   
+    if (pageYOffset >= sectionTop - window.innerHeight / 2 ) {
+      currents = section.getAttribute("id");  
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("nav-active");
+    if (li.classList.contains(currents)) {
+      li.classList.add("nav-active");
+    }
+  });
 });
 
